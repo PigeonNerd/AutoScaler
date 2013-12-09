d8b093b8afd09f1bb6ee760cbd031548f7625fa2
Qing Zheng <qingzhen@h0.zhengqds2.openstacksys.marmot.pdl.cmu.local>
2013-12-09 13:27:39 -0700

    add logs

--- rock.sh
+++ rock.sh
@@ -5,5 +5,5 @@ sudo rm -f /var/siege.log
 
 python AutoScalerCli.py instantiate /proj/OpenStackSys/proj2/scripts/autoscaler_templates/tomcat.template_1
 sleep 5
-sudo /proj/OpenStackSys/proj2/scripts/load_tests/load_test_2 http://localhost:8088/webserver/
+sudo /proj/OpenStackSys/group_data/test/load_test_3 http://localhost:8088/webserver/
 python AutoScalerCli.py destroy /proj/OpenStackSys/proj2/scripts/autoscaler_templates/tomcat.template_1
